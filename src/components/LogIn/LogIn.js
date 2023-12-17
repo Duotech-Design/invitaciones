f775738ab9f6ea50dc6d2b336f9b2a1cdcbf849f
RA-1124 <alison.rangelperez@gmail.com>
2023-12-17 15:01:34 -0600

    login

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -4,21 +4,25 @@ import Input from '@mui/joy/Input';
 import Stack from '@mui/joy/Stack';
 import { Box } from '@mui/material';
 import TextField from '@mui/material/TextField';
+import theme from '../../theme';
 
 
 export default function LogIn() {
   return (
+    <Box sx={{backgroundColor: '#D8DCDD', height:"80%"}}>
     <Box
     component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
+    sx={{ 
+      '& > :not(style)': { m: 1, width: '35ch' },
+      
     }}
     noValidate
     autoComplete="off"
   >
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <TextField id="filled-basic" label="Filled" variant="filled" />
+
+    <TextField label="Filled success" variant="filled" sx={{color:"#806B4B"}} focused />
     <TextField id="standard-basic" label="Standard" variant="standard" />
+  </Box>
   </Box>
   );
 }
