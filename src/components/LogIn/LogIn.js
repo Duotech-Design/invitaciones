4608d12dd09b8f28cf1aff779f0898c398dd8f7f
RA-1124 <alison.rangelperez@gmail.com>
2023-12-18 16:59:43 -0600

    login sin logo

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,29 +1,53 @@
-import * as React from 'react';
-import Button from '@mui/joy/Button';
-import Input from '@mui/joy/Input';
-import Stack from '@mui/joy/Stack';
-import { Box } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import theme from '../../theme';
+import * as React from "react";
+import {  Button, Box } from "@mui/material";
 
+import TextField from "@mui/material/TextField";
+
+
+const styles = {
+  button : {
+    //color: "D9D9D9",
+    //width: `clamp(2rem, 6rem, 8rem)`,
+    height: `clamp(1.8rem, 2.5rem, 3.5rem)`,
+    borderRadius: "40px",
+    backgroundColor: "#7D5730",
+    textAlign: "center",
+    fontFamily: "Cinzel",
+    //fontSize:`clamp(12px, 0.8rem, 0.9rem)`,
+    fontStyle: "normal",
+    fontWeight: "400px",
+    lineHeight: "normal",
+    marginBottom:"10px", 
+    color: "#F4EBE2",
+  },
+}
 
 export default function LogIn() {
   return (
-    <Box sx={{backgroundColor: '#D8DCDD', height:"80%"}}>
-    <Box
-    component="form"
-    sx={{ 
-      '& > :not(style)': { m: 1, width: '35ch' },
-      
-    }}
-    noValidate
-    autoComplete="off"
-  >
-
-    <TextField label="Filled success" variant="filled" sx={{color:"#806B4B"}} focused />
-    <TextField id="standard-basic" label="Standard" variant="standard" />
-  </Box>
-  </Box>
+    <Box sx={{ background: `url("/img/FondoLogin.svg")`, height: "100vh", display:"flex", justifyContent:"center" }}>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "35ch" },
+          display:"flex", justifyContent:"center", alignContent:"center", flexDirection:"column", alignItems:"center"
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField label="Correo" variant="filled" color="quinary" focused />
+        <TextField
+          label="Contraseña"
+          variant="filled"
+          color="quinary"
+          focused
+        />
+        <Button
+          variant="contained"
+          sx={{ ...styles.button, textAlign: "center" }}
+        >
+          INGRESAR
+        </Button>
+      </Box>
+    </Box>
   );
 }
-    
