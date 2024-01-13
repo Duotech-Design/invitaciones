effe8b2a3792ef80077885ba7ffe4dab050e9a96
RA-1124 <alison.rangelperez@gmail.com>
2024-01-12 21:39:47 -0600

    sugerencias maquillaje y peinado

--- src/components/Sugerencias/Sugerencias.js
+++ src/components/Sugerencias/Sugerencias.js
@@ -1,43 +1,46 @@
 //import React from 'react';
 import { Box, Container, Typography } from "@mui/material";
-import Link from '@mui/material/Link';
+import Link from "@mui/material/Link";
 
 const styles = {
   logo: {
-    maxWidth: "30%",
-    maxHeight: "30%"
-  }
-}
+    maxWidth: "45%",
+    maxHeight: "45%",
+  },
+  circle: {
+    width: "90px",
+    height: "90px",
+    borderRadius: "50%",
+    backgroundColor: "white",
+    position: "relative",
+    top: "10px", // Ajusta la posición vertical según tus necesidades
+  },
+};
 
 const Sugerencias = () => {
   return (
-    <Container   sx={{
-      height: "500px",
-      width: "100%",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center", // Para centrar verticalmente
-      alignItems: "center",
-      color: "white",
-      marginTop: "0",
-      marginBottom: "15px", 
-      gap:"10px"
-    }}>
+    <Container
+      sx={{
+        height: "600px",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center", // Para centrar verticalmente
+        alignItems: "center",
+        color: "white",
+        marginTop: "0",
+        marginBottom: "15px",
+        gap: "10px",
+      }}
+    >
       <Box>
         <div
           style={{ borderBottom: "0.3px solid #7D5730", width: "50vw" }}
         ></div>
       </Box>
       <Typography variant="horaP" sx={{ textAlign: "center" }}>
-        SUGERENCIAS 
+        SUGERENCIAS PEINADO Y MAQUILLAJE
       </Typography>
-      <div>
-      <Typography variant="horaP" sx={{ textAlign: "center" }}>
-      PEINADO Y MAQUILLAJE
-      </Typography>
-      </div>
-    
-
       <div
         style={{
           borderBottom: "0.3px solid #7D5730",
@@ -45,18 +48,78 @@ const Sugerencias = () => {
           marginTop: "5px",
         }}
       ></div>
-       <img
-          src="/img/6.png"
-          alt="Instagram" // Propiedad alt para describir la imagen
-          style={styles.logo}
-        />
-          <img
-          src="/img/6.1.png"
-          alt="Whatsapp" // Propiedad alt para describir la imagen
-          style={styles.logo}
-        />
+
+      {/* Contenedor de círculo e imágenes */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "10px",
+          width: "50vw",
+          justifyContent: "space-between"
+        }}
+      >
+        {/* Primer conjunto de círculo e imágenes */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "10px",
+            flex:"1"
+          }}
+        >
+          {/* Círculo blanco */}
+          <div style={styles.circle}></div>
+
+          {/* Imágenes */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "5px",
+              marginTop: "2px",
+            }}
+          >
+            <img src="/img/6.png" alt="Instagram" style={styles.logo} />
+            <img src="/img/6.1.png" alt="Whatsapp" style={styles.logo} />
+          </div>
+        </div>
+        <div style={{ borderLeft: "0.5px solid #7D5730", flex:"1" }}>
+          {/* Segundo conjunto de círculo e imágenes */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "10px",
+            }}
+          >
+            {/* Círculo blanco */}
+            <div style={styles.circle}></div>
+
+            {/* Imágenes */}
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "5px",
+                marginTop: "2px",
+              }}
+            >
+              {/* Imágenes con tamaño diferente */}
+              <img src="/img/6.png" alt="Instagram" style={styles.logo} />
+            <img src="/img/6.1.png" alt="Whatsapp" style={styles.logo} />
+            </div>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
-
 export default Sugerencias;
