7edfb15c82e7d0a4153d4833502e0bb13a0cb526
RA-1124 <alison.rangelperez@gmail.com>
2024-01-14 20:48:33 -0600

    cambios en letra de sugerencias hotel

--- src/components/Sugerencias/SugerenciaHotel.js
+++ src/components/Sugerencias/SugerenciaHotel.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Box, Container, Typography, Button, useTheme } from "@mui/material";
 import Link from '@mui/material/Link';
 
@@ -20,32 +21,31 @@ const styles = {
   },
   title: {
     textAlign: "center",
-    fontSize: "1rem",
+    fontSize: "2rem", // Tamaño de fuente para modo desktop
+    marginBottom: "5px",
+  },
+  mobileTitle: {
+    fontSize: "1rem", // Ajusta el tamaño de fuente para modo mobile
     marginBottom: "5px",
   },
   subtitle: {
     textAlign: "center",
-    fontSize: "1.5rem",
+    fontSize: "1.2rem",
     marginBottom: "10px",
-  },
-  weddingInfo: {
-    fontSize: "1rem", // Tamaño de fuente más pequeño para el texto de la boda
-    marginBottom: "10px",
-    textAlign: "center",
-  },
-  hotelInfo: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: "10px",
+    '@media (max-width: 599px)': {
+      fontSize: '0.9rem', // Tamaño de fuente para modo mobile
+    },
   },
   hotelName: {
     fontFamily: "Cinzel",
     fontWeight: "bold",
-    fontSize: "1rem",
-    color: "#7D5730", // Color café
+    fontSize: "1.5rem", // Tamaño de fuente para modo desktop
+    color: "#7D5730",
     marginBottom: "5px",
+    '@media (max-width: 599px)': {
+      fontSize: '1rem', // Tamaño de fuente para modo mobile
+    },
+  
   },
   hotelCode: {
     fontFamily: "Cinzel",
@@ -69,49 +69,61 @@ const styles = {
     fontWeight: "500",
     padding: "10px 15px",
   },
+  mobileTitle: {
+    fontSize: "1rem", // Ajusta el tamaño de fuente para modo mobile
+    marginBottom: "5px",
+    "@media (min-width:600px)": {
+      display: "none", // Oculta en modo desktop
+    },
+    mobileTitle: {
+      fontSize: "1rem", // Ajusta el tamaño de fuente para modo mobile
+      marginBottom: "5px",
+    },
+  },
 };
 
-const SugerenciasHotel = () => {
- 
 
+const SugerenciasHotel = () => {
   return (
     <Container sx={styles.container}>
       <Box sx={styles.divider} />
-      <Typography variant="horaP" sx={styles.title}>
+      <Typography
+        variant="horaP"
+        sx={{
+          ...styles.title,
+          '@media (max-width: 599px)': styles.mobileTitle,
+        }}
+      >
         SUGERENCIAS
-      </Typography>
-      <Typography variant="horaP" sx={styles.title}>
+        <br />
         HOTELES
       </Typography>
       <Box sx={styles.divider} />
-      <Typography variant="h3" sx={styles.weddingInfo}>
+      <Typography variant="h3" sx={styles.subtitle}>
         Mencionar boda de Andrea y Diego
       </Typography>
-      <div sx={styles.hotelInfo}>
+      <div sx={{...styles.hotelInfo, '@media (max-width: 599px)': {textAlign: 'center'}}}>
         <Typography variant="h1" sx={styles.hotelName}>
           CITY EXPRESS PLUS
-          <br/> 
+          <br /> 
           PLAZA TANGENTE
         </Typography>
         <Typography variant="hora" sx={{ marginBottom: "10px" }}></Typography>
-      <Typography variant="h11" sx={styles.address}>
-        PL. LOMAS LOTE 52,
-        <br />
-        LOMAS DEL TECNOLÓGICO,
-        <br />
-        San Luis Potosí, S.L.P.
-      </Typography>
+        <Typography variant="h11" sx={styles.address}>
+          PL. LOMAS LOTE 52,
+          <br />
+          LOMAS DEL TECNOLÓGICO,
+          <br />
+          San Luis Potosí, S.L.P.
+        </Typography>
       </div>
       <Typography variant="hora" sx={{ marginBottom: "5px" }}></Typography>
       <div sx={styles.hotelInfo}>
-      
         <Typography variant="h3" sx={styles.hotelCode}>
           Código : ANYDI
         </Typography>
       </div>
-      <Link
-        href="https://www.google.com/maps/place/City+Express+Plus+San+Luis+Potos%C3%AD/@22.1300382,-101.032409,19.12z/data=!4m8!3m7!1s0x842a99cc41533565:0xeb7f6233789f133d!5m2!4m1!1i2!8m2!3d22.1298168!4d-101.0322726?coh=164777&entry=tt&shorturl=1"
-      >
+      <Link href="https://www.google.com/maps/place/City+Express+Plus+San+Luis+Potos%C3%AD/@22.1300382,-101.032409,19.12z/data=!4m8!3m7!1s0x842a99cc41533565:0xeb7f6233789f133d!5m2!4m1!1i2!8m2!3d22.1298168!4d-101.0322726?coh=164777&entry=tt&shorturl=1">
         <Button sx={styles.mapButton}>VER MAPA</Button>
       </Link>
     </Container>
