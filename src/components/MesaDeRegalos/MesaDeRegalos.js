cf49721a9f2c732eb56c9cbb39c8053bdd9c3a63
RA-1124 <alison.rangelperez@gmail.com>
2024-01-14 22:48:18 -0600

    cambios acomodo mesa de regalos

--- src/components/MesaDeRegalos/MesaDeRegalos.js
+++ src/components/MesaDeRegalos/MesaDeRegalos.js
@@ -3,6 +3,32 @@ import { Box, Container, Typography } from "@mui/material";
 import Link from '@mui/material/Link';
 
 const styles = {
+  container: {
+    height: "auto",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    marginTop: "0",
+    marginBottom: "15px",
+    gap: "10px",
+  },
+  divider: {
+    borderBottom: "1px solid #7D5730",
+    width: "50vw",
+  },
+  title: {
+    marginTop: "30px",
+    marginBottom: "5px",
+    fontSize: "2rem",
+  },
+  subtitle: {
+    borderBottom: "1px solid #7D5730",
+    width: "50vw",
+    marginBottom: "5px",
+  },
   text: {
     display: "flex",
     flexDirection: "column",
@@ -15,94 +41,58 @@ const styles = {
     width: "100%",
   },
   imagenContenedor: {
-    position: "relative",  // Establecer la posición relativa
+    position: "relative",
     border: "1.2px solid #CBB197",
     borderRadius: "10px",
-    width: "50%",
+    width: "80%", // Ajustar el ancho según sea necesario
     margin: "auto",
     marginBottom: "5px",
     marginTop: "5px",
   },
   imagen: {
-    width: "95%",
+    width: "100%",
     height: "auto",
-    margin: "10px 0",  // Ajustar el margen vertical
-  }, // Ajusta el margen según sea necesario
-  };
-
+    margin: "10px 0",
+  },
+  desktopText: {
+    fontSize: "1rem", // Tamaño de letra ajustado para ambos contenedores
+    textAlign: "center",
+    margin: "5px 0",
+  },
+};
 
 const MesaDeRegalos = () => {
   return (
-    <Container
-      sx={{
-        height: "auto",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "white",
-        marginTop: "0",
-        marginBottom: "15px",
-        gap: "10px",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "30px",
-        }}
-      >
-        <div style={{ borderBottom: "1px solid #7D5730", width: "50vw" }}></div>
-      </Box>
-      <Typography variant="horaP">MESA DE REGALOS</Typography>
-      <div style={{ borderBottom: "1px solid #7D5730", width: "50vw" }}></div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: "10px",
-        }}
-      >
-        <Typography variant="h3">
-          Su presencia y compañía siempre serán nuestro
-        </Typography>
-        <Typography variant="h3">
-          mejor regalo. Sin embargo, si desean obsequiarnos
-        </Typography>
-        <Typography variant="h3">
-          algo más, ponemos a su disposición
-        </Typography>
-      </div>
+    <Container sx={styles.container}>
+      <Box sx={styles.divider} />
+      <Typography variant="horaP" sx={styles.title}>
+        MESA DE REGALOS
+      </Typography>
+      <Box sx={styles.divider} />
+      {/* Resto del código */}
       <Link href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51201601">
-      <div style={styles.imagenContenedor}>
-       
+        <div style={styles.imagenContenedor}>
           <img src="/img/7.png" alt="logo" width="100%" height="auto" style={styles.imagen} />
-       
-        <Box sx={styles.text}>
-          <Typography variant="h3">Núm. de Evento: 51201601</Typography>
-          <Link href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51201601">
-            <Typography variant="h11">VER MESA DE REGALOS</Typography>
-          </Link>
-        </Box>
-      </div>
+          <Box sx={{...styles.text, ...styles.desktopText}}>
+            <Typography variant="h3">Núm. de Evento: 51201601</Typography>
+            <Link href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51201601">
+              <Typography variant="h11">VER MESA DE REGALOS</Typography>
+            </Link>
+          </Box>
+        </div>
       </Link>
       <Link href="https://www.elpalaciodehierro.com/buscar?eventId=380437">
-      <div style={styles.imagenContenedor}>
-       
-          <img src="/img/7.1.png" alt="Imagen 1" width="95%" height="auto" style={styles.imagen} />
-      
-        <Box sx={styles.text}>
-          <Typography variant="h3">Núm. de Evento: 380437</Typography>
-          <Link href="https://www.elpalaciodehierro.com/buscar?eventId=380437">
-            <Typography variant="h11">VER MESA DE REGALOS</Typography>
-          </Link>
-        </Box>
-      </div>
+        <div style={{...styles.imagenContenedor, width: '80%'}}>
+          <img src="/img/7.1.png" alt="Imagen 1" width="100%" height="auto" style={styles.imagen} />
+          <Box sx={{...styles.text, ...styles.desktopText}}>
+            <Typography variant="h3">Núm. de Evento: 380437</Typography>
+            <Link href="https://www.elpalaciodehierro.com/buscar?eventId=380437">
+              <Typography variant="h11">VER MESA DE REGALOS</Typography>
+            </Link>
+          </Box>
+        </div>
       </Link>
+
       <div
         style={{
           padding: "20px",
