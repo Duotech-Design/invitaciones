bfdf2dfc18376e48a5e2021727b50c9125f149d8
RA-1124 <alison.rangelperez@gmail.com>
2024-01-14 22:19:31 -0600

    acomodo imagen sigerencia peinado!

--- src/components/Itinerario/Itinerario.js
+++ src/components/Itinerario/Itinerario.js
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import useMediaQuery from '@mui/material/useMediaQuery';
-import Opcio from "./Opcio";
+
 
 const styles = {
     container: {
@@ -167,7 +167,7 @@ const Itinerario = () => {
           </Box>
 
         </Box>
-        <Opcio/>
+    
       </div>
     </>
   );
